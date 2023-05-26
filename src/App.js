a562995f81b9af7d8f929ed125eb8c9f895a2a1b
Sovlus <kubbaz122@gmail.com>
2023-05-26 22:35:14 +0200

    imported router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
