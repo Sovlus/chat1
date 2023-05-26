a0b414bc66bba49a77dcfa11801212a797fb941a
Sovlus <kubbaz122@gmail.com>
2023-05-26 22:55:42 +0200

    added return

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function MessageForm({ name, sendMessage }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (text.trim() !== '') {
+    if (text.trim() !== "") {
       sendMessage(text);
-      setText('');
+      setText("");
     }
   };
+
+  return (
+    <form className='message-form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Type your message'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type='submit'>Send</button>
+    </form>
+  );
+}
+
+export default MessageForm;
