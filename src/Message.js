7f51835615c4d576fd5e74517f5cd0fd1e9b3dbe
Sovlus <kubbaz122@gmail.com>
2023-05-26 22:53:26 +0200

    added function

--- src/Message.js
+++ src/Message.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function Message({ message }) {
+  return (
+    <div className='message'>
+      <strong>{message.sender}:</strong> {message.text}
+    </div>
+  );
+}
+
+export default Message;
