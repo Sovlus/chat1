8e80170a9abaf48a7ef9dc126c83a01cae072573
Sovlus <kubbaz122@gmail.com>
2023-05-26 22:59:21 +0200

    created function user list

--- src/UserList.js
+++ src/UserList.js
@@ -1,2 +1,14 @@
 import React from "react";
 import User from "./User";
+
+function UserList({ users }) {
+  return (
+    <div className='user-list'>
+      {users.map((user, index) => (
+        <User key={index} user={user} />
+      ))}
+    </div>
+  );
+}
+
+export default UserList;
